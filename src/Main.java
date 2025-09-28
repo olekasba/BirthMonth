c397e542f1a3575b5dee5b6c438a6c1554c336d8
olekasba <olekasba@mail.uc.edu>
2025-09-28 12:14:15 -0400

    Finalized Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner in = new Scanner(System.in);
+        System.out.print("What is your birth month as an interger between 1 and 12?");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        if(in.hasNextInt())
+        {
+
+            int birthMonth = in.nextInt();
+            if(birthMonth >= 1 && birthMonth <= 12) {
+                System.out.println("Your birth month is: " + birthMonth);
+            }
+            else {
+                System.out.println("You entered an incorrect month value: " + birthMonth );
+            }
+        }
+        else
+        {
+            String birthMonth = in.nextLine();
+            System.out.println("You entered an incorrect month value: " + birthMonth );
         }
     }
+
 }
